15c283313eca1cdf6163913df51c9f966d06ef21
Trankhanh0605 <khanh.tran0605@hcmut.edu.vn>
2025-07-11 23:03:54 +0700

    update

--- code.cpp
+++ code.cpp
@@ -1,27 +1,8 @@
 #include <iostream>
 using namespace std; 
 int main () {
-    int count=0; 
-    while (count<8) {
-        if (count%2==0) {
-            int i=0; 
-            while (i<8) {
-                cout << "* ";
-                cout << ' ';
-                i++;
-            }
-            cout<< endl; 
-        }
-        else {
-            int i=0; 
-            while (i<8) {
-                cout << ' ';
-                cout << "* ";
-                i++;
-            }
-            cout<< endl; 
-        }
-        count++;
+    for (int i=0; i<10; i++) {
+        cout<< i; 
     }
     return 0;
 }
